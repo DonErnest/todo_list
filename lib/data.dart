7aa9bb2bc55154a69375f3c472fd6f97ffce9116
Ernest Aitiev <valeetmihifaveas@gmail.com>
2025-02-07 23:11:49 +0600

    added TaskCategory model and added taskCategory id to the Task model. Also added four basic categories and refactored default tasks

--- lib/data.dart
+++ lib/data.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
+class TaskCategory {
+  final int id;
+  final String name;
+  final IconData icon;
+
+  TaskCategory({required this.id, required this.name, required this.icon});
+}
+
 class Task {
   final String description;
+  final int categoryId;
   bool done;
   DateTime deadLine;
   DateTime? completedOn;
 
-  Task({required this.description, this.done = false, required this.deadLine});
+  Task( {required this.description, required this.categoryId, this.done = false, required this.deadLine});
 }
 
 String _zeroPad(int dateTimeValue) {
@@ -39,25 +48,46 @@ String formatTime(TimeOfDay dateTime) {
   return '$hour:$minute';
 }
 
+var shoppingCategory = TaskCategory(id:1, name: "Покупки", icon: Icons.shopping_bag);
+var meetingCategory = TaskCategory(id: 2, name: "Встречи", icon: Icons.handshake_outlined);
+var workCategory = TaskCategory(id: 3, name: "Работа", icon: Icons.work_outline);
+var adventureCategory = TaskCategory(id: 4, name: "Приключение", icon: Icons.route);
+
+var categories = [
+  shoppingCategory,
+  meetingCategory,
+  workCategory,
+  adventureCategory
+];
+
+TaskCategory getCategoryById(int id) {
+  return categories.firstWhere((category) => category.id == id);
+}
+
 List<Task> tasks = [
   Task(
     description: "купить молоко",
+    categoryId: shoppingCategory.id,
     deadLine: DateTime.now().add(Duration(days: 1)),
   ),
   Task(
     description: "захватить мир",
+    categoryId: adventureCategory.id,
     deadLine: DateTime.now().subtract(Duration(days: 1)),
   ),
   Task(
     description: "найти смысл жизни",
+    categoryId: adventureCategory.id,
     deadLine: DateTime.now().add(Duration(days: 15000)),
   ),
   Task(
     description: "подумать",
+    categoryId: workCategory.id,
     deadLine: DateTime.now().add(Duration(hours: 1)),
   ),
   Task(
     description: "подготовиться к зиме",
+    categoryId: meetingCategory.id,
     deadLine: DateTime.now().subtract(Duration(days: 70)),
   ),
 ];
