776c13c0097d5cdc212cffe136f264d4b131cbb6
Ernest Aitiev <valeetmihifaveas@gmail.com>
2025-03-06 21:27:55 +0600

    Added regulation of dark/bright themes and set dark theme as default

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:todo_list/todo.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: TodoList(),
+    MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      home: const TodoList(),
     ),
   );
 }
